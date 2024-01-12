6684ab2a3698333b0683c5e11b3d4e9d9ba55aa2
ziord <jeremiahikosin@gmail.com>
2024-01-12 10:52:23 -0500

    update zig script

--- getzig.sh
+++ getzig.sh
@@ -2,14 +2,24 @@
 curr_d=$(pwd)
 ZIG_VERSION="0.12.0-dev.1604+caae40c21"
 BUILD_MACHINE_ARCH=$(arch)
-ZIG_FOLDERNAME="zig-linux-${BUILD_MACHINE_ARCH}-${ZIG_VERSION}"
+platform=$(uname -s)
+case $platform in
+    Linux*)     platform="linux" ;;
+    Darwin*)    platform="macos" ;;
+    *)          platform="Unknown" ;;
+esac
+if [[ $BUILD_MACHINE_ARCH == *arm* ]]; then
+    BUILD_MACHINE_ARCH=x86_64
+fi
+ZIG_FOLDERNAME="zig-${platform}-${BUILD_MACHINE_ARCH}-${ZIG_VERSION}"
 ZIG_FILENAME="${ZIG_FOLDERNAME}.tar.xz"
 ZIG_URL="https://ziglang.org/builds/${ZIG_FILENAME}"
 
-if ! command -v zig &> /dev/null || "$(zig --version)" != $ZIG_VERSION; then
+if ! command -v zig &> /dev/null || "$(zig version)" != $ZIG_VERSION; then
     # download and extract Zig
     TEMP=$(mktemp -d)
     mkdir $TEMP && cd $TEMP
+    echo "Fetching zig from: $ZIG_URL"
     wget "$ZIG_URL"
     tar xf "$ZIG_FILENAME"
     # install Zig
